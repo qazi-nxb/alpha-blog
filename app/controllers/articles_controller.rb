6a51f44517442102b35c864e4e20c3ac211b7c77
Faheem <qazi.faheem@nxb.com.pk>
2021-03-12 17:56:06 +0500

    show & create with full error handlings

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,25 @@
 class ArticlesController < ApplicationController
+  def show
+    @article= Article.find(params[:id])
+  end
+  
+  def index
+    @article = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to(@article)
+    if @article.save
+      flash[:notice] = "Added Successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+
   end
 
   def article_params
