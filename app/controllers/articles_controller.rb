8613cf441b5290b40fccd3234d873ca93392edde
Faheem <qazi.faheem@nxb.com.pk>
2021-03-16 20:36:30 +0500

    styling of show and errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Added Successfully"
+      flash[:success] = "Added Successfully"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Updated successfully"
+      flash[:success] = "Updated successfully"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   def destroy
     #binding.pry
     @article.destroy
-    flash[:notice]="Article was deleted successfully"
+    flash[:danger]="Article was deleted successfully"
     redirect_to articles_path
   end
   private
