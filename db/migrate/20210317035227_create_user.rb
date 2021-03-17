4b1a6417d83533a777391550551d7ed70db3feb5
Faheem <qazi.faheem@nxb.com.pk>
2021-03-17 09:02:20 +0500

    create user

---
+++ db/migrate/20210317035227_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :useremail
+
+      t.timestamps
+    end
+  end
+end
