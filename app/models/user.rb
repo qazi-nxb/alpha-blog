ef7f54082da0817537f22e87f502a72dcaec42da
Faheem <qazi.faheem@nxb.com.pk>
2021-03-17 10:36:49 +0500

    Validation on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {minimum: 3}
+  VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :useremail, presence: true,
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_REGEX}
 end
